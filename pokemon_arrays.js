f54a9344483f0b877545e7468faabd64fdb776a1
RAYTRAC3R <samipiplup@gmail.com>
2025-02-14 18:21:13 -0600

    Begin work

--- pokemon_arrays.js
+++ pokemon_arrays.js
@@ -1,99 +1,20 @@
 var arrays = {
-	 "Grass": [
+	 "V1": [
 	  [],
 	  [
-	   "BULBASAUR",
-	   "IVYSAUR",
-	   "VENUSAUR",
-	   "ODDISH",
-	   "GLOOM",
-	   "VILEPLUME",
-	   "PARAS",
-	   "PARASECT",
-	   "BELLSPROUT",
-	   "WEEPINBELL",
-	   "VICTREEBEL",
-	   "EXEGGCUTE",
-	   "EXEGGUTOR",
-	   "TANGELA"
+        "LOLA",
+        "MIRIAM"
+      ],
+	  [
+        "LEON"
 	  ],
 	  [
-	   "CHIKORITA",
-	   "BAYLEEF",
-	   "MEGANIUM",
-	   "BELLOSSOM",
-	   "HOPPIP",
-	   "SKIPLOOM",
-	   "JUMPLUFF",
-	   "SUNKERN",
-	   "SUNFLORA",
-	   "CELEBI"
+        "MEIKOV1"
 	  ],
 	  [
-	   "TREECKO",
-	   "GROVYLE",
-	   "SCEPTILE",
-	   "LOTAD",
-	   "LOMBRE",
-	   "LUDICOLO",
-	   "SEEDOT",
-	   "NUZLEAF",
-	   "SHIFTRY",
-	   "SHROOMISH",
-	   "BRELOOM",
-	   "ROSELIA",
-	   "CACNEA",
-	   "CACTURNE",
-	   "LILEEP",
-	   "CRADILY",
-	   "TROPIUS"
+        "KAITOV1"
 	  ],
 	  [
-	   "TURTWIG",
-	   "GROTLE",
-	   "TORTERRA",
-	   "BUDEW",
-	   "ROSERADE",
-	   "WORMADAM",
-	   "CHERUBI",
-	   "CHERRIM",
-	   "CHERRIM_1",
-	   "CARNIVINE",
-	   "SNOVER",
-	   "ABOMASNOW",
-	   "TANGROWTH",
-	   "LEAFEON",
-	   "ROTOM_5",
-	   "SHAYMIN",
-	   "SHAYMIN_1"
-	  ],
-	  [
-	   "SNIVY",
-	   "SERVINE",
-	   "SERPERIOR",
-	   "PANSAGE",
-	   "SIMISAGE",
-	   "SEWADDLE",
-	   "SWADLOON",
-	   "LEAVANNY",
-	   "COTTONEE",
-	   "WHIMSICOTT",
-	   "PETILIL",
-	   "LILLIGANT",
-	   "MARACTUS",
-	   "DEERLING",
-	   "DEERLING_1",
-	   "DEERLING_2",
-	   "DEERLING_3",
-	   "SAWSBUCK",
-	   "SAWSBUCK_1",
-	   "SAWSBUCK_2",
-	   "SAWSBUCK_3",
-	   "FOONGUS",
-	   "AMOONGUSS",
-	   "FERROSEED",
-	   "FERROTHORN",
-	   "VIRIZION"
 	  ],
 	  [
 	   "CHESPIN",
@@ -166,65 +87,19 @@ var arrays = {
 	   "OGERPON"
 	  ]
 	 ],
-	 "Poison": [
+	 "V2": [
 	  [],
 	  [
-	   "BULBASAUR",
-	   "IVYSAUR",
-	   "VENUSAUR",
-	   "WEEDLE",
-	   "KAKUNA",
-	   "BEEDRILL",
-	   "EKANS",
-	   "ARBOK",
-	   "NIDORANfE",
-	   "NIDORINA",
-	   "NIDOQUEEN",
-	   "NIDORANmA",
-	   "NIDORINO",
-	   "NIDOKING",
-	   "ZUBAT",
-	   "GOLBAT",
-	   "ODDISH",
-	   "GLOOM",
-	   "VILEPLUME",
-	   "VENONAT",
-	   "VENOMOTH",
-	   "BELLSPROUT",
-	   "WEEPINBELL",
-	   "VICTREEBEL",
-	   "TENTACOOL",
-	   "TENTACRUEL",
-	   "GRIMER",
-	   "MUK",
-	   "GASTLY",
-	   "HAUNTER",
-	   "GENGAR",
-	   "KOFFING",
-	   "WEEZING"
 	  ],
 	  [
-	   "SPINARAK",
-	   "ARIADOS",
-	   "CROBAT",
-	   "QWILFISH"
+        "BIGAL"
 	  ],
 	  [
-	   "DUSTOX",
-	   "ROSELIA",
-	   "GULPIN",
-	   "SWALOT",
-	   "SEVIPER"
 	  ],
 	  [
-	   "BUDEW",
-	   "ROSERADE",
-	   "STUNKY",
-	   "SKUNTANK",
-	   "SKORUPI",
-	   "DRAPION",
-	   "CROAGUNK",
-	   "TOXICROAK"
+        "LENV2",
+        "GACKPOIDV2",
+        "VY2V2"
 	  ],
 	  [
 	   "VENIPEDE",
@@ -3471,7 +3346,7 @@ var arrays = {
 var array_of_forms = ["GENESECT","GENESECT_1","GENESECT_2","GENESECT_3","GENESECT_4","LUNALA","LUNALA_1","SOLGALEO","SOLGALEO_1","MARSHADOW","MARSHADOW_1","MIMIKYU","MIMIKYU_1","ZACIAN","ZACIAN_2","ZAMAZENTA","ZAMAZENTA_2","ZARUDE","ZARUDE_1","PIKACHU","PIKACHU_2","PIKACHU_3","PIKACHU_4","PIKACHU_5","PIKACHU_6","PIKACHU_7","PIKACHU_8","PIKACHU_9","PIKACHU_10","PIKACHU_11","PIKACHU_12","PIKACHU_13","PIKACHU_14","PIKACHU_15","ABOMASNOW","ABOMASNOW_female","ALAKAZAM","ALAKAZAM_female","AIPOM","AIPOM_female","AMBIPOM","AMBIPOM_female","BEAUTIFLY","BEAUTIFLY_female","BIBAREL","BIBAREL_female","BIDOOF","BIDOOF_female","BLAZIKEN","BLAZIKEN_female","BUTTERFREE","BUTTERFREE_female","CACTURNE","CACTURNE_female","CAMERUPT","CAMERUPT_female","COMBEE","COMBEE_female","COMBUSKEN","COMBUSKEN_female","CROAGUNK","CROAGUNK_female","DODRIO","DODRIO_female","DODUO","DODUO_female","DONPHAN","DONPHAN_female","DUSTOX","DUSTOX_female","FINNEON","FINNEON_female","GARCHOMP","GARCHOMP_female","GIBLE","GIBLE_female","GIRAFARIG","GIRAFARIG_female","GLIGAR","GLIGAR_female","GLOOM","GLOOM_female","GOLBAT","GOLBAT_female","GOLDEEN","GOLDEEN_female","GULPIN","GULPIN_female","GYARADOS","GYARADOS_female","HERACROSS","HERACROSS_female","HOUNDOOM","HOUNDOOM_female","HYPNO","HYPNO_female","KADABRA","KADABRA_female","KRICKETOT","KRICKETOT_female","KRICKETUNE","KRICKETUNE_female","LEDIAN","LEDIAN_female","LEDYBA","LEDYBA_female","LUDICOLO","LUDICOLO_female","LUMINEON","LUMINEON_female","LUXIO","LUXIO_female","LUXRAY","LUXRAY_female","MAGIKARP","MAGIKARP_female","MAMOSWINE","MAMOSWINE_female","MEDICHAM","MEDICHAM_female","MEDITITE","MEDITITE_female","MEGANIUM","MEGANIUM_female","MILOTIC","MILOTIC_female","NUMEL","NUMEL_female","NUZLEAF","NUZLEAF_female","OCTILLERY","OCTILLERY_female","PIKACHU","PIKACHU_female","PILOSWINE","PILOSWINE_female","POLITOED","POLITOED_female","QUAGSIRE","QUAGSIRE_female","RAICHU","RAICHU_female","RATICATE","RATICATE_female","RATTATA","RATTATA_female","RELICANTH","RELICANTH_female","RHYDON","RHYDON_female","RHYHORN","RHYHORN_female","RHYPERIOR","RHYPERIOR_female","ROSELIA","ROSELIA_female","ROSERADE","ROSERADE_female","SCIZOR","SCIZOR_female","SCYTHER","SCYTHER_female","SEAKING","SEAKING_female","SHIFTRY","SHIFTRY_female","SHINX","SHINX_female","SNEASEL","SNEASEL_female","SNEASEL_1","SNEASEL_1_female","SNOVER","SNOVER_female","STARAPTOR","STARAPTOR_female","STARAVIA","STARAVIA_female","STARLY","STARLY_female","STEELIX","STEELIX_female","SUDOWOODO","SUDOWOODO_female","SWALOT","SWALOT_female","TANGROWTH","TANGROWTH_female","TOXICROAK","TOXICROAK_female","URSARING","URSARING_female","VENUSAUR","VENUSAUR_female","VILEPLUME","VILEPLUME_female","WEAVILE","WEAVILE_female","WOBBUFFET","WOBBUFFET_female","WOOPER","WOOPER_female"];
 
 var rev_map = {}
-var type_count_array = ["Grass", "Poison", "Fire", "Flying", "Dragon", "Water", "Bug",
+var type_count_array = ["V1", "V2", "Fire", "Flying", "Dragon", "Water", "Bug",
 	"Normal", "Dark", "Electric", "Psychic", "Ground", "Ice",
 	"Steel", "Fairy", "Fighting", "Rock", "Ghost"]
 for(var type in type_count_array) {
